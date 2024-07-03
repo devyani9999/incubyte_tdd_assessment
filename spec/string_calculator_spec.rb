5e17d5449b04e9184b9823431fa33bd647c8668f
devyani9999 <devyani12034@iiitd.ac.in>
2024-07-03 09:02:38 +0530

    1. string_calculator.rb #add handles single number case

--- spec/string_calculator_spec.rb
+++ spec/string_calculator_spec.rb
@@ -4,8 +4,16 @@ describe StringCalculator do
   describe ".add" do
     context "for empty string" do
       it "returns 0" do
-        result =  StringCalculator.add("")
-        expect(result).to eq(0)
+        expect(StringCalculator.add("")).to eq(0)
+      end
+    end
+
+    context "for single number" do
+      it "returns the number itself" do
+        expect(StringCalculator.add("1")).to eq(1)
+        expect(StringCalculator.add("5")).to eq(5)
+        expect(StringCalculator.add("10")).to eq(10)
+        expect(StringCalculator.add("15")).to eq(15)
       end
     end
   end
