db7b1de5b12e7ebd85b0def0945b7213d23d8c0e
devyani9999 <devyani12034@iiitd.ac.in>
2024-07-03 13:00:31 +0530

    1. string_calculator.rb and string_calculator_spec.rb refactoring

--- lib/string_calculator.rb
+++ lib/string_calculator.rb
@@ -1,20 +1,40 @@
 # frozen_string_literal: true
 
 class StringCalculator
-  def self.add(input)
+  def add(input)
     input.strip!
     if input.empty?
-      return 0
-    elsif input.start_with?("//")
-      input = input.split("\n")
-      delimiter = input[0][-1]
-      numbers = input[1]
-      numbers = numbers.split(delimiter).map(&:to_i)
-      return numbers.sum
+      0
     else
-      input = input.split(/[,\n]/).map(&:to_i)
-      return input.sum
+      sanitized_numbers = get_sanitized_numbers(input)
+      sanitized_numbers.sum
     end
   end
 
+  private
+
+  def custom_delimiter?(input)
+    input.start_with?("//")
+  end
+
+  def get_sanitized_numbers(input)
+    delimiters = /[,\n]/
+    if custom_delimiter?(input)
+      numbers = custom_numbers(input)
+    else
+      numbers = input.split(delimiters)
+    end
+
+    numbers.map(&:to_i)
+  end
+
+  def custom_numbers(input)
+    input = input.split("\n")
+    delimiter = custom_delimiter(input[0])
+    input[1].split(delimiter)
+  end
+
+  def custom_delimiter(input)
+    input[-1]
+  end
 end
--- spec/string_calculator_spec.rb
+++ spec/string_calculator_spec.rb
@@ -2,53 +2,56 @@ require "string_calculator"
 
 describe StringCalculator do
   describe ".add" do
+    let(:string_calculator) { StringCalculator.new }
+
     context "for empty string" do
       it "returns 0" do
-        expect(StringCalculator.add("")).to eq(0)
+        expect(string_calculator.add("")).to eq(0)
       end
     end
 
     context "for single number" do
       it "returns the number itself" do
-        expect(StringCalculator.add("1")).to eq(1)
-        expect(StringCalculator.add("5")).to eq(5)
-        expect(StringCalculator.add("10")).to eq(10)
-        expect(StringCalculator.add("15")).to eq(15)
+        expect(string_calculator.add("1")).to eq(1)
+        expect(string_calculator.add("5")).to eq(5)
+        expect(string_calculator.add("10")).to eq(10)
+        expect(string_calculator.add("15")).to eq(15)
       end
     end
 
     context "for two numbers" do
       it "returns the sum of two numbers" do
-        expect(StringCalculator.add("1,5")).to eq(6)
-        expect(StringCalculator.add("5,2")).to eq(7)
-        expect(StringCalculator.add("10,3")).to eq(13)
-        expect(StringCalculator.add("15,4")).to eq(19)
+        expect(string_calculator.add("1,5")).to eq(6)
+        expect(string_calculator.add("5,2")).to eq(7)
+        expect(string_calculator.add("10,3")).to eq(13)
+        expect(string_calculator.add("15,4")).to eq(19)
       end
     end
 
     context "for multiple numbers" do
       it "returns the sum of all the numbers" do
-        expect(StringCalculator.add("1,5,6,7")).to eq(19)
-        expect(StringCalculator.add("5,2,5,6")).to eq(18)
-        expect(StringCalculator.add("10,3,2,3")).to eq(18)
-        expect(StringCalculator.add("15,4,8,9")).to eq(36)
+        expect(string_calculator.add("1,5,6,7")).to eq(19)
+        expect(string_calculator.add("5,2,5,6")).to eq(18)
+        expect(string_calculator.add("10,3,2,3")).to eq(18)
+        expect(string_calculator.add("15,4,8,9")).to eq(36)
       end
     end
 
     context "for multiple numbers with , and '\\n'" do
       it "returns the sum of all the numbers" do
-        expect(StringCalculator.add("1\n2,3")).to eq(6)
-        expect(StringCalculator.add("5,2\n5\n6")).to eq(18)
-        expect(StringCalculator.add("10,3\n2,3")).to eq(18)
-        expect(StringCalculator.add("15\n4\n8\n9")).to eq(36)
+        expect(string_calculator.add("1\n2,3")).to eq(6)
+        expect(string_calculator.add("5,2\n5\n6")).to eq(18)
+        expect(string_calculator.add("10,3\n2,3")).to eq(18)
+        expect(string_calculator.add("15\n4\n8\n9")).to eq(36)
       end
     end
 
     context "for multiple numbers with delimiter and numbers separated by \\n: '//[delimiter]\\n[numbers…]'" do
       it "returns the sum of all the numbers" do
-        expect(StringCalculator.add("//;\n1;2")).to eq(3)
-        expect(StringCalculator.add("//:\n2:3:4")).to eq(9)
+        expect(string_calculator.add("//;\n1;2")).to eq(3)
+        expect(string_calculator.add("//:\n2:3:4")).to eq(9)
       end
     end
+
   end
 end
